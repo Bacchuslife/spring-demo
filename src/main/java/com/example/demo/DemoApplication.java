2a4cba18c0d5bac79ff41b50481c4fd770d2e5d2
Bacchus <Bacchus@test.com>
2020-06-02 09:50:00 +0800

    @

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -11,18 +10,4 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-}
-
-@Component
-class Foo {
-	int e;
-	
-	public Foo() {
-		this.e = 88;
-		System.out.println("Foo.Foo()");
-	}
-	
-	public Foo(int e) {
-		this.e = e;
-	}
 }
